2494054073cd0e9de98ff0ef7e7455e27e1e0055
Sakisakthi <124359398+Sakisakthi@users.noreply.github.com>
2023-09-02 10:52:52 +0530

    slider

--- js.js
+++ js.js
@@ -35,3 +35,16 @@ function myFunction() {
 	window.scrollBy(0, 8300);
 	
   }
+
+
+
+/* slider in card yoga icons*/
+  $('.carousel .carousel-item').each(function () {
+	var minPerSlide = 4;
+	var next = $(this).next();
+	if (!next.length) {
+	next = $(this).siblings(':first');
+	}
+	next.children(':first-child').clone().appendTo($(this));
+	
+	for (var i = 0; i < minPerSlide; i++) { next=next.next(); if (!next.length) { next=$(this).siblings(':first'); } next.children(':first-child').clone().appendTo($(this)); } });
