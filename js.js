10033635a05d934f21dc984db0ed7726c6612adb
Sakisakthi <124359398+Sakisakthi@users.noreply.github.com>
2023-10-15 19:37:25 +0530

    Update js.js

--- js.js
+++ js.js
@@ -26,7 +26,31 @@ if (!$(".goto").hasClass("animated")) {
 /*  join now button scrool down */
 
 function myFunction() {
-	window.scrollBy(0, 3500);
+	$(document).ready(function(){
+		// Add smooth scrolling to all links
+		$("myFunction").on('click', function(event) {
+	  
+		  // Make sure this.hash has a value before overriding default behavior
+		  if (this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
+	  
+			// Store hash
+			var hash = this.hash;
+	  
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+			  scrollBy: $(hash).offset().top
+			}, 3500, function(){
+		 
+			  // Add hash (#) to URL when done scrolling (default click behavior)
+			  window.location.hash = hash;
+			});
+		  } // End if
+		});
+	  });
+	// window.scrollBy(0, 3500);
 	
   }
 
